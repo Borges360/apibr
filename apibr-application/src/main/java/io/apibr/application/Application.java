171a87ff413c98aa400732c1ae485495da81c6d5
Luiz Felipe <felipeborgeseng@outlook.com.br>
2021-05-08 18:09:24 -0300

    Profile maven, dockerização da aplicação e swagger

--- apibr-application/src/main/java/io/apibr/application/Application.java
+++ apibr-application/src/main/java/io/apibr/application/Application.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @ComponentScan(basePackages = {"io.apibr.cep/*"})
 @EnableJpaRepositories(basePackages = {"io.apibr.cep.adapters.repository"})
 @EntityScan(basePackages = {"io.apibr.cep.adapters.data"})
 @EnableAutoConfiguration
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
